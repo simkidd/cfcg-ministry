b536c164f5c43994e373b205702403497935b586
John Mason <87030324+simkidd@users.noreply.github.com>
2024-02-07 14:43:06 +0100

    chore: bible verses slider

---
+++ src/data/verses.data.ts
@@ -0,0 +1,20 @@
+export const bibleVerses =[
+  {
+    book: "John",
+    chapter: "1",
+    verse: "18",
+    text: "Little children, let us not love in word or talk but in deed and in truth",
+  },
+  {
+    book: "John",
+    chapter: "3",
+    verse: "16-17",
+    text: "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life. For God did not send his Son into the world to condemn the world, but in order that the world might be saved through him.",
+  },
+  {
+    book: "Matthew",
+    chapter: "6",
+    verse: "24",
+    text: "No one can serve two masters, for either he will hate the one and love the other, or he will be devoted to the one and despise the other. You cannot serve God and money.",
+  },
+]

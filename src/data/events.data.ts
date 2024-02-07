24091de787bdcce5f6f5896734e10e6f8e7fd4aa
John Mason <87030324+simkidd@users.noreply.github.com>
2024-02-07 19:03:47 +0100

    chore: added gallery and event section

---
+++ src/data/events.data.ts
@@ -0,0 +1,12 @@
+export const events = [
+  {
+    title: "",
+    time: "",
+    venue: "",
+    image:
+      "../assets/images/gallery/WhatsApp Image 2024-02-07 at 10.23.14.jpeg",
+    content: "",
+  },
+  { title: "", time: "", venue: "", image: "", content: "" },
+  { title: "", time: "", venue: "", image: "", content: "" },
+];
